38fd7677265b12da0cc7057e6c5611f3f1474516
0xrsydn <rafayrussani@gmail.com>
2024-09-03 17:09:48 +0700

    layout modifying

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,9 +7,35 @@ export default {
     		borderRadius: {
     			lg: 'var(--radius)',
     			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
+    			sm: 'calc(var(--radius) - 4px)',
+                base: '5px'
     		},
-    		colors: {}
+    		colors: {
+                main: '#FD9745',
+                mainAccent: '#fc7303',
+                overlay: 'rgba(0,0,0,0.8)',
+                bg: '#fff4e0',
+                text: '#000',
+                border: '#000',
+                darkBg: '#1D1F27',
+                darkText: '#eeefe9',
+                darkBorder: '#000',
+                secondaryBlack: '#1b1b1b',
+            },
+            boxShadow: {
+                light: '4px 4px 0px 0px #000',
+                dark: '4px 4px 0px 0px #000',
+            },
+            translate: {
+                boxShadowX: '4px',
+                boxShadowY: '4px',
+                reverseBoxShadowX: '-4px',
+                reverseBoxShadowY: '-4px',
+            },
+            fontWeight: {
+                base: '500',
+                heading: '700',
+            },
     	}
     },
 	plugins: [require("tailwindcss-animate")],
