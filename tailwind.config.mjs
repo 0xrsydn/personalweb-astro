c6acfe7b379acac0d1f6bc72baf2b77551c26ebc
0xrsydn <rafayrussani@gmail.com>
2024-09-04 10:55:57 +0700

    editing layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,40 +3,41 @@ export default {
     darkMode: ['class'],
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-    	extend: {
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)',
-                base: '5px'
-    		},
-    		colors: {
-                main: '#FD9745',
-                mainAccent: '#fc7303',
-                overlay: 'rgba(0,0,0,0.8)',
-                bg: '#fff4e0',
-                text: '#000',
-                border: '#000',
-                darkBg: '#1D1F27',
-                darkText: '#eeefe9',
-                darkBorder: '#000',
-                secondaryBlack: '#1b1b1b',
-            },
-            boxShadow: {
-                light: '4px 4px 0px 0px #000',
-                dark: '4px 4px 0px 0px #000',
-            },
-            translate: {
-                boxShadowX: '4px',
-                boxShadowY: '4px',
-                reverseBoxShadowX: '-4px',
-                reverseBoxShadowY: '-4px',
-            },
-            fontWeight: {
-                base: '500',
-                heading: '700',
-            },
-    	}
-    },
+		extend: {
+		  colors: {
+			main: '#FFDC58',
+			mainAccent: '#ffc800', // not needed for shadcn components
+			overlay: 'rgba(0,0,0,0.8)', // background color overlay for alert dialogs, modals, etc.
+	  
+			// light mode
+			bg: '#FEF2E8',
+			text: '#000',
+			border: '#000',
+	  
+			// dark mode
+			darkBg: '#374151',
+			darkText: '#eeefe9',
+			darkBorder: '#000',
+			secondaryBlack: '#1b1b1b', // opposite of plain white, not used pitch black because borders and box-shadows are that color 
+		  },
+		  borderRadius: {
+			base: '5px'
+		  },
+		  boxShadow: {
+			light: '4px 4px 0px 0px #000',
+			dark: '4px 4px 0px 0px #000',
+		  },
+		  translate: {
+			boxShadowX: '4px',
+			boxShadowY: '4px',
+			reverseBoxShadowX: '-4px',
+			reverseBoxShadowY: '-4px',
+		  },
+		  fontWeight: {
+			base: '500',
+			heading: '700',
+		  },
+		},
+	  },
 	plugins: [require("tailwindcss-animate")],
 }
